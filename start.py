cf7ebd44c1b20cd4b09e32df47e97b178eeda61a
LuckyMagpie <diego.sse7@gmail.com>
2015-04-20 21:54:19 -0500

    Update

--- start.py
+++ start.py
@@ -4,18 +4,28 @@ from sys import argv
 import Metro
 import Estacion
 
+estaciones = []
+metros = []
 master = Tk()
+
+def test(m, canvas):
+    m.mover(canvas, 40, 0)
+    master.after(1000, test,m,canvas)
 
 canvas = Canvas(master, width=1280, height=680)
 canvas.pack()
 canvas.create_line(0, 680/2, 1280, 680/2, dash=(30), width=3.0)
-Metro.Metro(1,1,1).dibujar(canvas)
-m = Estacion.Estacion()
-m.dibujar(canvas, 'Estacion 1')
+
+for i in xrange(0, 4):
+    estaciones.append(Estacion.Estacion())
+    estaciones[i].dibujar(canvas, 'Estación '+ str(i+1))
+    estaciones[i].mover(canvas, 340 * i, 0)
 
 
 
-
+m = Metro.Metro(1,1,1)
+m.dibujar(canvas)
+master.after(1000, test, m, canvas)
 
 
 mainloop()
